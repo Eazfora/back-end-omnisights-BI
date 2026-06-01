5ecc38d29583bee7590e1baa79599c662b39d08f
Eazfora <agunzprogrammer@gmail.com>
2026-06-01 18:57:28 +0700

    enablecors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: '*', // Untuk tahap development/sidang, tanda '*' artinya mengizinkan semua domain (termasuk Amplify kamu)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
